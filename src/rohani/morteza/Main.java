a8bf51acb9c973d5978fc7672366c95972438de5
rohanimorteza <rohani.morteza@gmail.com>
2020-03-01 19:10:05 +0330

    session-03 part-2 Relational, Assignment and Logical Operators

--- src/rohani/morteza/Main.java
+++ src/rohani/morteza/Main.java
@@ -4,29 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*
-        short a , b , c;
-        a = 127;
-        b = 100;
-        c = a + b ; error
-        */
+        int a = 12 , b =10;
+        //System.out.println(a!=b);
+        a += 10;
+        System.out.println(a);
 
-        /*
-        byte a = 10;
-        char c = '2';
-        System.out.println(a+2);
-        */
 
-        /*
-        byte a = 5 , b =1;
-        System.out.println(5/2); // please check result
-        */
 
-        byte  a = 5 , b =1;
-        b++;
-        System.out.println(5/++b);
-        System.out.println(b++);
-        System.out.println(b);
 
     }
 }
