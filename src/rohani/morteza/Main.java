bab703c76323fb6e1f3c24d04950075c1a5170fd
rohanimorteza <rohani.morteza@gmail.com>
2020-03-01 18:54:15 +0330

    session-03 part-1 Data Type and Arithmetic Operators

--- src/rohani/morteza/Main.java
+++ src/rohani/morteza/Main.java
@@ -4,7 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        /*
+        short a , b , c;
+        a = 127;
+        b = 100;
+        c = a + b ; error
+        */
+
+        /*
+        byte a = 10;
+        char c = '2';
+        System.out.println(a+2);
+        */
+
+        /*
+        byte a = 5 , b =1;
+        System.out.println(5/2); // please check result
+        */
+
+        byte  a = 5 , b =1;
+        b++;
+        System.out.println(5/++b);
+        System.out.println(b++);
+        System.out.println(b);
 
     }
 }
